ca4ef595241ab906ea5364c55ef856017a34cb52
Irahel <isaac_rahel@live.com>
2023-02-11 12:26:40 -0300

    feat: base api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+use rocket::{get, http::Status, serde::json::Json};
+
+#[get("/")]
+fn hello() -> Result<Json<String>, Status> {
+    Ok(Json(String::from("Hello from rust and mongoDB")))
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![hello])
 }
