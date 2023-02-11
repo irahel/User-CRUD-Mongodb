9b9da2b5ee549b150487341f464813c89231a90e
Irahel <isaac_rahel@live.com>
2023-02-11 12:39:08 -0300

    feat: add user

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
+mod api;
+mod models;
+mod repository;
+
 #[macro_use]
 extern crate rocket;
 use rocket::{get, http::Status, serde::json::Json};
 
-#[get("/")]
-fn hello() -> Result<Json<String>, Status> {
-    Ok(Json(String::from("Hello from rust and mongoDB")))
-}
+use api::user_api::create_user;
+use repository::mongodb_repo::MongoRepo;
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+    let db = MongoRepo::init();
+    rocket::build().manage(db).mount("/", routes![create_user])
 }
